f5a0009b8b2d91fa8002032a4bc305cff2403806
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-11-14 08:51:37 -0500

    El contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using JoseRivera_AP1_P2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JoseRivera_AP1_P2.DAL
+{
+    public class Contexto:DbContext
+    {
+        public Contexto(DbContextOptions<Contexto>options) :base(options){ }
+        public DbSet<Registros> Registros { get; set; }
+    }
+}
