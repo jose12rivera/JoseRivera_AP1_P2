890012bc231949c85a55f84bcd312f28b96ba45f
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-11-14 08:49:47 -0500

    El Modelos Registros

---
+++ Models/Registros.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace JoseRivera_AP1_P2.Models;
+
+public class Registros
+{
+    [Key]
+    public int RegistroId { get; set; }
+    [Required(ErrorMessage ="Por Favor llenar el Campos")]
+    public string? Nombre{ get; set; }
+    [Required(ErrorMessage = "Por Favor llenar el Campos")]
+    public string? Descripcion { get;set; }
+}
