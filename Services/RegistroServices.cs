9e5ac6025765de1f0d58a2cff1d768712ec23874
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-11-14 09:08:18 -0500

    El services

---
+++ Services/RegistroServices.cs
@@ -0,0 +1,11 @@
+﻿using JoseRivera_AP1_P2.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace JoseRivera_AP1_P2.Services
+{
+    public class RegistroServices(IDbContextFactory<Contexto> DbFactory)
+    {
+
+        //await using var contexto = await DbFactory.CreateDbContextAsync();
+    }
+}
