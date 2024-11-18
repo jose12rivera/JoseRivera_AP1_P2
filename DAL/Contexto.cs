5250d1db99d8ac03439d70791b4980bfdf6d03a1
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-11-18 17:16:43 -0500

    aqui comienzo

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -6,5 +6,17 @@ namespace JoseRivera_AP1_P2.DAL;
 public class Contexto:DbContext
 {
     public Contexto(DbContextOptions<Contexto>options) :base(options){ }
-    public DbSet<Registros> registros { get; set; }
+    public DbSet<Articulos> Articulos{ get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Articulos>().HasData(new List<Articulos>() {
+         new Articulos() { ArticuloId=1,Nombre="Mouse"},
+
+
+
+
+        } );
+    }
 }
---
+++ Models/Articulos.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JoseRivera_AP1_P2.Models
+{
+    public class Articulos
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+        [Required(ErrorMessage ="Intentar de nuevo")]
+        public string? Nombre { get; set; }
+    }
+}
