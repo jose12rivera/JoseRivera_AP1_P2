93d49f12145a34d10e90590401947f50098e49bf
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-11-18 20:05:48 -0500

    aqui voy en el examen

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -13,7 +13,8 @@ public class Contexto:DbContext
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<Articulos>().HasData(new List<Articulos>() {
+        modelBuilder.Entity<Articulos>().HasData(new List<Articulos>()
+        {
          new Articulos() { ArticuloId=1,Nombre="Mouse",Descripcion="Del"},
          new Articulos() { ArticuloId=2,Nombre="Ram",Descripcion="64 GB"},
          new Articulos() { ArticuloId=3,Nombre="Teclado",Descripcion="Gamer 7!"},
